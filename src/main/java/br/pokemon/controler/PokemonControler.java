8e31b78f683593706260f90bbb1bd11a86868f03
Marcelo Campos Silva <Marcelo.Campos@ntconsult.net>
2020-02-28 14:19:14 -0300

    :sparkles: adicionado dto de retorno no metodo listaPokemon

--- src/main/java/br/pokemon/controler/PokemonControler.java
+++ src/main/java/br/pokemon/controler/PokemonControler.java
@@ -1,6 +1,5 @@
 package br.pokemon.controler;
 
-import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.pokemon.modelo.Pokemon;
+import br.pokemon.dto.PokemonDto;
 import br.pokemon.servico.PokemonServico;
 
 @RestController
@@ -20,8 +19,8 @@ public class PokemonControler {
 	private PokemonServico pokemonServico;
 	
 	@GetMapping
-	public ResponseEntity<List<Pokemon>> listaPokemon() throws IOException {
-		List<Pokemon> lista = pokemonServico.listaPokemons();
+	public ResponseEntity<List<PokemonDto>> listaPokemon() {
+		List<PokemonDto> lista = pokemonServico.listaPokemons();
 		if (lista.isEmpty()) {
 			return ResponseEntity.notFound().build();
 		}
--- src/main/java/br/pokemon/servico/PokemonServico.java
+++ src/main/java/br/pokemon/servico/PokemonServico.java
@@ -1,10 +1,12 @@
 package br.pokemon.servico;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.pokemon.dto.PokemonDto;
 import br.pokemon.modelo.Pokemon;
 import br.pokemon.repositorio.PokemonRepositorio;
 
@@ -14,8 +16,14 @@ public class PokemonServico {
 	@Autowired
 	private PokemonRepositorio pokemonRepositorio;		
 		
-	public List<Pokemon> listaPokemons(){
-		return pokemonRepositorio.findAll();
+	public List<PokemonDto> listaPokemons(){
+		List<PokemonDto> listaPokemons = new ArrayList<>();
+		pokemonRepositorio.findAll().forEach(p -> listaPokemons.add(convertePokemonDto(p)));		
+		return listaPokemons;
+	}
+	
+	private PokemonDto convertePokemonDto(Pokemon pokemon) {
+		return new PokemonDto(pokemon);		
 	}
 
 }
