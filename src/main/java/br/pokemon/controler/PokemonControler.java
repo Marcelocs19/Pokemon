a7298f794146a0455227afd061f85794c5228e06
Marcelo Campos Silva <Marcelo.Campos@ntconsult.net>
2020-03-05 15:13:07 -0300

    :fire: Removendo filtro de tipos

--- src/main/java/br/pokemon/controler/PokemonControler.java
+++ src/main/java/br/pokemon/controler/PokemonControler.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.pokemon.dto.PokemonDto;
@@ -20,12 +19,12 @@ public class PokemonControler {
 	private PokemonServico pokemonServico;
 	
 	@GetMapping
-	public ResponseEntity<List<PokemonDto>> listaPokemon(@RequestParam(required = false) String tipo1, @RequestParam(required = false) String tipo2) {
-		List<PokemonDto> lista = pokemonServico.listaPokemons(tipo1, tipo2);
+	public ResponseEntity<List<PokemonDto>> listaPokemon() {
+		List<PokemonDto> lista = pokemonServico.listaPokemons();
 		if (lista.isEmpty()) {
 			return ResponseEntity.notFound().build();
 		}
 		return ResponseEntity.ok().body(lista);
-	}		
+	}	
 	
 }
--- src/main/java/br/pokemon/servico/PokemonServico.java
+++ src/main/java/br/pokemon/servico/PokemonServico.java
@@ -6,11 +6,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.pokemon.configurar.LeituraTxt;
 import br.pokemon.dto.PokemonDto;
 import br.pokemon.modelo.Pokemon;
 import br.pokemon.repositorio.PokemonRepositorio;
-import br.pokemon.tipo.Tipo;
 
 @Service
 public class PokemonServico {
@@ -18,32 +16,11 @@ public class PokemonServico {
 	@Autowired
 	private PokemonRepositorio pokemonRepositorio;
 
-	public List<PokemonDto> listaPokemons(String tipo1, String tipo2) {
+	public List<PokemonDto> listaPokemons() {
 		List<PokemonDto> listaPokemons = new ArrayList<>();
-		if (tipo1 != null || tipo2 != null) {
-			return buscaTipo(tipo1, tipo2, listaPokemons);
-		}
 		pokemonRepositorio.findAll().forEach(p -> listaPokemons.add(convertePokemonDto(p)));
 		return listaPokemons;
-	}
-
-	private List<PokemonDto> buscaTipo(String tipo1, String tipo2, List<PokemonDto> listaPokemons) {
-		List<Pokemon> buscaTipo;
-		if(tipo1 != null && tipo2 == null) {
-			Tipo tipoPokemon1 = LeituraTxt.tipoPokemon(tipo1);
-			buscaTipo = pokemonRepositorio.findByTipo1(tipoPokemon1);
-		} else if(tipo1 == null && tipo2 != null) {
-			Tipo tipoPokemon2 = LeituraTxt.tipoPokemon(tipo2);
-			buscaTipo = pokemonRepositorio.findByTipo2(tipoPokemon2);
-		} else {
-			Tipo tipoPokemon1 = LeituraTxt.tipoPokemon(tipo1);
-			Tipo tipoPokemon2 = LeituraTxt.tipoPokemon(tipo2);
-			buscaTipo = pokemonRepositorio.findByTipo1AndTipo2(tipoPokemon1,tipoPokemon2);
-		}
-		
-		buscaTipo.forEach(p -> listaPokemons.add(convertePokemonDto(p)));		
-		return listaPokemons;
-	}
+	}	
 
 	private PokemonDto convertePokemonDto(Pokemon pokemon) {
 		return new PokemonDto(pokemon);
