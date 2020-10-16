45013f20148ce381ae262b64900f1aa25ca426ef
Marcelo Campos <marcelo.home19@gmail.com>
2020-10-16 09:55:28 -0300

    ✨ criando arquivo de data.sql para o banco h2

--- src/main/java/br/pokemon/configuracoes/Inicializacao.java
+++ src/main/java/br/pokemon/configuracoes/Inicializacao.java
@@ -3,12 +3,8 @@ package br.pokemon.configuracoes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import br.pokemon.enums.Perfil;
-import br.pokemon.modelos.Usuario;
 import br.pokemon.repositorios.PokemonRepositorio;
-import br.pokemon.repositorios.UsuarioRepositorio;
 
 @Configuration
 public class Inicializacao implements CommandLineRunner {
@@ -16,26 +12,12 @@ public class Inicializacao implements CommandLineRunner {
 	@Autowired
 	private PokemonRepositorio pokemonRepositorio;	
 	
-	@Autowired
-	private UsuarioRepositorio usuarioRepositorio;
-	
-	@Autowired
-	private BCryptPasswordEncoder pe;
-	
 	@Override
 	public void run(String... args) throws Exception {
 	
 		pokemonRepositorio.deleteAll();
 		
 		pokemonRepositorio.saveAll(LeituraTxt.leitura());
-		
-//		Usuario admin = new Usuario();
-//		admin.setApelido("admin");
-//		admin.setNome("Marcelo");
-//		admin.setSenha(pe.encode("123"));
-//		admin.setEmail("marcelo@gmail.com");
-//		admin.addPerfil(Perfil.ADMIN);		
-//		usuarioRepositorio.saveAndFlush(admin);
 		
 	}
 	
--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,3 +1,4 @@
-INSERT INTO USUARIO(id, nome,email,apelido,senha) VALUES(1, 'Marcelo','marcelo@gmail.com', 'admin', '123') ON CONFLICT DO NOTHING;
+--Senha 123
+INSERT INTO USUARIO(id, nome,email,apelido,senha) VALUES(1, 'Marcelo','marcelo@gmail.com', 'admin', '$2a$10$hm0eYRoAK3r2Mf3O30dDmOxShNZTLyM8QahF7cc38SYnrJYroNnSm') ON CONFLICT DO NOTHING;
 
 INSERT INTO PERFIL(usuario_id,perfis) VALUES(1,1) ON CONFLICT DO NOTHING;
