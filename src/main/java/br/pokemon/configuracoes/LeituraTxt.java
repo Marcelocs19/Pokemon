2b08098d9926c20636c08f838f7e5927b14bd310
Marcelo <marcelo.home19@gmail.com>
2020-11-25 14:04:32 -0300

    ♻️ Refatorado código de leitura de arquivo

--- src/main/java/br/pokemon/configuracoes/LeituraTxt.java
+++ src/main/java/br/pokemon/configuracoes/LeituraTxt.java
@@ -1,9 +1,9 @@
 package br.pokemon.configuracoes;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -15,55 +15,52 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class LeituraTxt {
 	
-	private static String path = "src/main/resources/Lista Pokemons.txt";
+	private static String caminho = "src/main/resources/Lista Pokemons.txt";
 	
 	private List<Tipo> listaTipos = Arrays.asList(Tipo.ACO, Tipo.AGUA, Tipo.DRAGAO, Tipo.ELETRICO, Tipo.FADA, Tipo.FANTASMA, Tipo.FOGO,
 			Tipo.GELO, Tipo.GRAMA, Tipo.INSETO, Tipo.LUTADOR, Tipo.NORMAL, Tipo.NOTURNO, Tipo.PEDRA, Tipo.PEDRA,
 			Tipo.PSIQUICO, Tipo.TERRA, Tipo.VENENOSO, Tipo.VOADOR);
 
 
-	public List<Pokemon> leitura() throws IOException {
-		File fileReader = new File(path);
+	public List<Pokemon> leitura() throws IOException {		
 		
 		List<Pokemon> listaPokemonMap = new ArrayList<>();
 		
-		var tipo1 = "";
-		var tipo2 = "";
-		var descricao = "";
+        Path path = Paths.get(caminho);
 		
-		try (FileReader fr = new FileReader(fileReader); BufferedReader bufferedReader = new BufferedReader(fr)) {
+        Files.lines(path).forEach(linha -> {
+        	var tipo1 = "";
+    		var tipo2 = "";
+    		var descricao = "";
+    		
+        	String[] arrayAux = new String[4];
+			arrayAux = linha.split(";");
 
-			while (bufferedReader.ready()) {
-				var line = bufferedReader.readLine();
+			StringBuilder aux = new StringBuilder();
 
-				String[] arrayAux = new String[4];
-				arrayAux = line.split(";");
+			Arrays.asList(arrayAux).forEach(i -> aux.append(i));				
 
-				StringBuilder aux = new StringBuilder();
-
-				Arrays.asList(arrayAux).forEach(i -> aux.append(i));				
-
-				tipo1 = arrayAux[2].replace("tipo1:", "");
-				if (aux.toString().contains("tipo2:")) {
-					tipo2 = arrayAux[3].replace("tipo2:", "");
-					descricao = arrayAux[4].replace("descricao:", "");
-				} else {
-					tipo2 = tipo1;
-					descricao = arrayAux[3].replace("descricao:", "");
-				}
-
-				Pokemon pokemon = new Pokemon();
-				
-				pokemon.setId(Long.parseLong(arrayAux[0].substring(3)));
-				pokemon.setNome(arrayAux[1].replace("nome:", ""));
-				pokemon.setDescricao(descricao);
-				pokemon.setTipo1(tipoPokemon(tipo1));
-				pokemon.setTipo2(tipoPokemon(tipo2));
-				
-				listaPokemonMap.add(pokemon);
+			tipo1 = arrayAux[2].replace("tipo1:", "");
+			if (aux.toString().contains("tipo2:")) {
+				tipo2 = arrayAux[3].replace("tipo2:", "");
+				descricao = arrayAux[4].replace("descricao:", "");
+			} else {
+				tipo2 = tipo1;
+				descricao = arrayAux[3].replace("descricao:", "");
 			}
-		}
 
+			var pokemon = new Pokemon();
+			
+			pokemon.setId(Long.parseLong(arrayAux[0].substring(3)));
+			pokemon.setNome(arrayAux[1].replace("nome:", ""));
+			pokemon.setDescricao(descricao);
+			pokemon.setTipo1(tipoPokemon(tipo1));
+			pokemon.setTipo2(tipoPokemon(tipo2));
+			
+			listaPokemonMap.add(pokemon);
+		
+        });
+        
 		return listaPokemonMap;
 
 	}
